efc0ac6dcb5c65adb6044866841ecebf82883b1c
canvassed <tydickson255@gmail.com>
2026-05-05 20:55:27 -0400

    Initial Commit

---
+++ activityapp/activityappApp.swift
@@ -0,0 +1,17 @@
+//
+//  activityappApp.swift
+//  activityapp
+//
+//  Created by Ty Dickson on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct activityappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ activityappTests/activityappTests.swift
@@ -0,0 +1,17 @@
+//
+//  activityappTests.swift
+//  activityappTests
+//
+//  Created by Ty Dickson on 5/5/26.
+//
+
+import Testing
+@testable import activityapp
+
+struct activityappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
