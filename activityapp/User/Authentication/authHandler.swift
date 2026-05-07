33b6424aa5791fd672b940461009e4b4e05e28bd
canvassed <tydickson255@gmail.com>
2026-05-06 20:53:34 -0400

    Start supabase implementation

---
+++ activityapp/User/Authentication/authHandler.swift
@@ -0,0 +1,7 @@
+//
+//  authHandler.swift
+//  activityapp
+//
+//  Created by Ty Dickson on 5/6/26.
+//
+
--- activityapp/activityappApp.swift
+++ activityapp/activityappApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Supabase
 
 @main
 struct activityappApp: App {
+    
+    @Environment var supabase = SupabaseClient(
+        supabaseURL: URL(string: "https://YOUR_PROJECT.supabase.co")!,
+        supabaseKey: "YOUR_ANON_KEY"
+    )
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
