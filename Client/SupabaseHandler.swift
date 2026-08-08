423a9202af7eace6fd29b1e5a3573c1a76fd3dd7
canvassed <tydickson255@gmail.com>
2026-08-08 12:44:52 +0900

    Server bug fix for update

--- Client/SupabaseHandler.swift
+++ Client/SupabaseHandler.swift
@@ -17,7 +17,7 @@ enum SupabaseHandler
     
     static let productionBackendURL: String = "https://activityapp-backend.fly.dev"
     
-    /*
+    
     #if DEBUG
     static let backendURL = localBackendURL
     static let supabaseURL = "https://coeythfyfwzrwzuqowfe.supabase.co"
@@ -27,12 +27,12 @@ enum SupabaseHandler
     static let supabaseURL = "https://vdxqfhrsuhmqdbpeqtbt.supabase.co"
     static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkeHFmaHJzdWhtcWRicGVxdGJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMzcwMzMsImV4cCI6MjA5MzYxMzAzM30.6My-s4mropLOelDfcl2KSQAX2gQI2QP0y4Db0pcLICg"
     #endif
-     */
+     /*
     
     static let backendURL = localBackendURL
     static let supabaseURL = "https://vdxqfhrsuhmqdbpeqtbt.supabase.co"
     static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkeHFmaHJzdWhtcWRicGVxdGJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMzcwMzMsImV4cCI6MjA5MzYxMzAzM30.6My-s4mropLOelDfcl2KSQAX2gQI2QP0y4Db0pcLICg"
-    
+    */
     static let client = SupabaseClient(
         supabaseURL: URL(string: supabaseURL)!,
         supabaseKey: supabaseKey
